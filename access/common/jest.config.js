1273a1aadfa691f012ca801634c92c032db5c1d4
Mike Cluck <mike-cluck@pluralsight.com>
2021-07-02 23:21:20 -0600

    Update Jest config to only test relevant files

--- access/common/jest.config.js
+++ access/common/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['src'],
   // TODO: Do this mapping automatically
   moduleNameMapper: {
     '@utility/common(.*)': '<rootDir>/../../utility/common/src$1',
--- access/todo/jest.config.js
+++ access/todo/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['src'],
   moduleNameMapper: {
     '@access/common(.*)': '<rootDir>/../common/src$1',
     '@utility/common(.*)': '<rootDir>/../../utility/common/src$1',

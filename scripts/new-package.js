189cc295ced7814fd939fd943e7b18b0b4f60622
Mike Cluck <mike-cluck@pluralsight.com>
2021-07-06 08:00:45 -0600

    Improve output from new package script

--- scripts/new-package.js
+++ scripts/new-package.js
@@ -1,15 +1,31 @@
 const { execSync } = require('child_process')
 const fs = require('fs-extra')
 const path = require('path')
+const logSymbols = require('log-symbols')
 
 const types = ['access', 'engine', 'manager', 'utility']
 
 const usage = () => console.error(`Usage: yarn new [type] [name]`)
 
+function log(msg) {
+  process.stdout.write(msg)
+}
+
+function logSucceed(msg) {
+  msg = msg || ''
+  console.log(`${msg} ${logSymbols.success}`)
+}
+
+function logFail(msg) {
+  msg = msg || ''
+  console.log(`${msg} ${logSymbols.error}`)
+}
+
 async function addToCodeWorkspace(projectRoot, workspaceType, type, name) {
   const packageName = `@${type}/${name}`
   const workspaceFileName = `${workspaceType}.code-workspace`
 
+  log(`Adding to ${workspaceFileName}`)
   try {
     const packagePath = `${type}/${name}`
 
@@ -32,9 +48,11 @@ async function addToCodeWorkspace(projectRoot, workspaceType, type, name) {
     execSync(
       `yarn prettier --write --parser json ${workspaceType}.code-workspace`,
     )
+    logSucceed()
   } catch (e) {
-    console.error(`Error while adding ${packageName} to ${workspaceFileName}`)
+    logFail(`Error while adding ${packageName} to ${workspaceFileName}`)
     console.error(e)
+    process.exit(1)
   }
 }
 
@@ -52,8 +70,10 @@ async function main() {
     process.exit(1)
   }
 
+  const packageName = `@${type}/${name}`
+  const workspaceRoot = path.join(__dirname, '..')
   try {
-    const packageName = `@${type}/${name}`
+    log(`Creating ${packageName}`)
     const templateRoot = path.join(__dirname, '../.config/package-template')
     const packageRoot = path.join(__dirname, '..', type, name)
     await fs.copy(templateRoot, packageRoot)
@@ -75,7 +95,6 @@ async function main() {
     )
 
     // Add to workspace
-    const workspaceRoot = path.join(__dirname, '..')
     const workspacePackagePath = path.join(workspaceRoot, 'package.json')
     const workspacePackage = require(workspacePackagePath)
     workspacePackage.workspaces.packages.push(`${type}/${name}`)
@@ -91,8 +110,16 @@ async function main() {
       JSON.stringify(workspacePackage, null, 2),
     )
 
-    execSync('yarn install', { cwd: workspaceRoot })
+    execSync('yarn install', { cwd: workspaceRoot, stdio: 'ignore' })
+    logSucceed()
+  } catch (err) {
+    logFail()
+    console.log(err)
+    return
+  }
 
+  try {
+    log('Configuring Jest')
     // Add mapping to jest.config.js
     const jestConfigPath = path.join(workspaceRoot, '.config/jest.config.js')
     const jestConfig = require(jestConfigPath)
@@ -118,22 +145,25 @@ async function main() {
     execSync('yarn prettier --write package.json .config/jest.config.js', {
       cwd: workspaceRoot,
     })
-
-    const typeToWorkspaces = {
-      access: ['backend', 'manager', 'engine', 'access'],
-      engine: ['backend', 'manager', 'engine', 'access'],
-      manager: ['backend', 'manager'],
-      utility: ['app', 'backend', 'manager', 'engine', 'access'],
-    }
-
-    for (const wp of typeToWorkspaces[type]) {
-      await addToCodeWorkspace(workspaceRoot, wp, type, name)
-    }
-
-    console.log(`Created ${packageName}`)
-  } catch (err) {
-    console.log(err)
+    logSucceed()
+  } catch (e) {
+    logFail()
+    console.error(e)
+    return
   }
+
+  const typeToWorkspaces = {
+    access: ['backend', 'manager', 'engine', 'access'],
+    engine: ['backend', 'manager', 'engine', 'access'],
+    manager: ['backend', 'manager'],
+    utility: ['app', 'backend', 'manager', 'engine', 'access'],
+  }
+
+  for (const wp of typeToWorkspaces[type]) {
+    await addToCodeWorkspace(workspaceRoot, wp, type, name)
+  }
+
+  logSucceed(`Created ${packageName}`)
 }
 
 main()

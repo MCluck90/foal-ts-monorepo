bad6b2dc6359dc9b5d5c3343b9a4a29de926dbfc
Mike Cluck <mike-cluck@pluralsight.com>
2021-07-09 16:53:36 -0600

    Fix build issue caused by including tests in tsconfig

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -12,4 +12,9 @@ module.exports = {
     '@manager/new-server(.*)': '<rootDir>/../../manager/new-server/src$1',
     '@utility/common(.*)': '<rootDir>/../../utility/common/src$1',
   },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 }

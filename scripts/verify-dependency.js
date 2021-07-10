d3655de6597281980620a585cfcb674345ef6218
Mike Cluck <mike-cluck@pluralsight.com>
2021-07-10 10:38:27 -0600

    Fix issue in dependency verification when installing third-party packages

--- scripts/verify-dependency.js
+++ scripts/verify-dependency.js
@@ -3,10 +3,16 @@
  */
 const packageName = process.env.npm_package_name
 const configArgv = JSON.parse(process.env.npm_config_argv)
-const dependencyName = configArgv.original[1]
+const dependencyName = configArgv.original
+  .slice(1)
+  .find((arg) => !arg.startsWith('-'))
 
 const getScope = (name) => {
-  const scope = name.match(/@([^/]+)/)[1]
+  const match = name.match(/@([^/]+)/)
+  if (!match) {
+    return name
+  }
+  const scope = match[1]
   if (scope === 'app') {
     return name.slice(1).split('@')[0]
   }
@@ -24,7 +30,12 @@ const allowedScopeLookup = {
 const packageScope = getScope(packageName)
 const dependencyScope = getScope(dependencyName)
 const allowedScopes = allowedScopeLookup[packageScope]
-if (allowedScopes && !allowedScopes.includes(dependencyScope)) {
+const isLocalDependency = !!allowedScopeLookup[dependencyScope]
+if (
+  allowedScopes &&
+  isLocalDependency &&
+  !allowedScopes.includes(dependencyScope)
+) {
   const allowedPackageTypes = allowedScopes.map((s) => `@${s}`).join(', ')
   console.error(
     `Cannot add @${dependencyScope} packages to @${packageScope} package(s).`,
